4463dbaca11b2bdd990b92dfa9ea82e3f1b571fe
WorkStation <XaJIaC@example.com>
2024-02-09 16:47:16 +0300

    mobile menu done

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,1 +1,13 @@
-const mobileButtonMenu=document.querySelector(".menu__toggle");mobileButtonMenu.addEventListener("click",()=>{mobileButtonMenu.classList.toggle("menu__toggle--open")});
+const mobileButtonMenu = document.querySelector(".header__mobile-toggle");
+const headerBorder = document.querySelector(".header__container");
+
+mobileButtonMenu.addEventListener("click",()=>{
+  if(mobileButtonMenu.classList.contains("header__mobile-toggle--open")) {
+    mobileButtonMenu.classList.remove("header__mobile-toggle--open");
+    headerBorder.classList.add("header__container--border");
+  }
+  else {
+    mobileButtonMenu.classList.add("header__mobile-toggle--open");
+    headerBorder.classList.remove("header__container--border");
+  }
+});
